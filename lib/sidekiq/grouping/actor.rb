322c760c1ffac450cf39c93cb491878f0e1e4a5d
Mark Haussmann <mark.haussmann@gmail.com>
2015-12-10 14:52:53 +1300

    Sidekiq no longer uses Celluloid so we cannot link the actor

--- lib/sidekiq/grouping/actor.rb
+++ lib/sidekiq/grouping/actor.rb
@@ -29,7 +29,7 @@ module Sidekiq
       end
 
       def link_to_sidekiq_manager
-        Sidekiq::CLI.instance.launcher.manager.link(current_actor)
+        Sidekiq::CLI.instance.launcher.manager
         start_polling
       rescue NoMethodError
         debug "Can't link #{self.class.name}. Sidekiq::Manager not running. Retrying in 5 seconds ..."
